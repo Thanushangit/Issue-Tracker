a8e2b7c61e6de6c02f9cd70ea43246b483fd7acf
Thanushan <sinnarasathanushan2002@gmail.com>
2025-12-09 22:44:04 +0530

    Latest issue page work

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import { Button } from "@radix-ui/themes";
-import Pagination from "./components/Pagination";
+import LatestIssuePage from "./LatestIssuePage";
 
-export default async function Home({searchParams}: {searchParams: {page?: string}}) {
-
-  const searchparamsNew = await searchParams;
-  return (
-    <div>
-      <Pagination itemCount={100} pageSize={10} currentPage={searchparamsNew.page ? parseInt(searchparamsNew.page) : 1} />
-    </div>
-  );
+export default async function Home() {
+ 
+  return <LatestIssuePage />;
 }
